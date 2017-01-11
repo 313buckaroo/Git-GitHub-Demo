1c8ee6e0f5559ca1b9c4e01bcc1d6f0fa98c49c3
Rob Blevins <Rob.313@live.com>
2017-01-11 12:28:18 -0800

    Add project files.

---
+++ Git-GitHub-Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Git_GitHub_Demo.Startup))]
+namespace Git_GitHub_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
