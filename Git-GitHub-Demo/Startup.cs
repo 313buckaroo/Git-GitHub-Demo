c7a68252414497c29072e52da34e7c67db0bacc1
Rob Blevins <Rob.313@live.com>
2017-01-11 13:08:53 -0800

    Initial Set Up

--- Git-GitHub-Demo/Startup.cs
+++ Git-GitHub-Demo/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Owin;
 using Owin;
-
+//I can do all things through Christ who strengthens me (Phillipians 4:13).
+//Even Learn C#, Azure, Git, Github, Visual Studio, Asp.Net, SQL, Wordpress, and Web Development.
 [assembly: OwinStartupAttribute(typeof(Git_GitHub_Demo.Startup))]
 namespace Git_GitHub_Demo
 {
